b2911185f8e859184952d3ff747b697c29d89545
LiliaRibac <ribac.lilia@gmai.com>
2019-07-10 14:53:55 -0500

    add style for btn

--- kanban-clone/src/components/KanbanActionButton.js
+++ kanban-clone/src/components/KanbanActionButton.js
@@ -6,9 +6,18 @@ class KanbanActionButton extends React.Component{
 renderAddButton = () =>{
     const {list} = this.props;
 
-    const buttonText = list ? "Add another list" : "Add another card"
+    const buttonText = list ? "Add another list" : "Add another card";
+    const btnTextOpacity = list ? 1 : 0.5;
+    const btnTextColor = list ? "white" : "inherit";
+    const btnTextBackground = list ? "rgba  (0,0,0,.15)" :"inherit";
+
     return (
-        <div>
+        <div style ={{
+            ...styles.openBtn,
+            opacity:btnTextOpacity,
+            color:btnTextColor,
+            backgroundColor:btnTextColor
+        }}>
             <Icon>add</Icon>
             <p>{buttonText}</p>
         </div>
@@ -17,6 +26,18 @@ renderAddButton = () =>{
 
     render(){
         return this.renderAddButton();
+    }
+}
+
+const styles ={
+    openBtn:{
+        display:"flex",
+        alignItems:"center",
+        cursor:"pointer",
+        borderRadius:3,
+        height:35,
+        width:270,
+        paddingLeft:10
     }
 }
 
